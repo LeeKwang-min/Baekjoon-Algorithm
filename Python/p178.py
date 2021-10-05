6e2d8b7663fba9d52d08a742714b278adfd05076
LeeSangsu <dltkdtn56@naver.com>
2021-10-05 11:37:37 +0900

    NDB P.178 위에서 아래로

---
+++ Python/p178.py
@@ -0,0 +1,19 @@
+n = int(input())
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+array = sorted(array, reverse=True)
+
+for i in array:
+    print(i, end=' ')
+
+
+'''
+3
+15
+27
+12
+
+// 27 15 12
+'''
